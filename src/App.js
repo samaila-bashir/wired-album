117ae421a4a02527bf17b72b9b212d1df997cf85
Samaila Chatto Bashir <chattobashir@gmail.com>
2022-02-22 17:16:07 +0100

    Implemented Material UI with styled components

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import Album from './components/albums/album-component';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
 import Photos from './components/photos/photos-component';
+import Navbar from "./components/navbar/Navbar-component";
 
 function App() {
   return (
     <Router>
       <div className="homepage">
+        <Navbar />
         <Switch>
           <Route exact path="/">
             <Album />
